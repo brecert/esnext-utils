1819ccea2da22c255459eb91381d47563987b049
bree <11599528+Brecert@users.noreply.github.com>
2020-12-08 21:01:55 -0500

    Create math.js

---
+++ src/math.js
@@ -0,0 +1,9 @@
+// @esnext use https://github.com/tc39/proposal-bind-operator
+// @esnext use https://github.com/tc39/proposal-iterator-helpers
+
+import { range, map, product } from './iterator.js'
+
+export const comb = (n, k) =>
+  range(1, k)
+    ::map((i) => (n + 1 - i) / i)
+    ::product();
