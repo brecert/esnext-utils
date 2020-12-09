4f064a47f05741bfdffe4ac4274b111d5bbf7a33
bree <11599528+Brecert@users.noreply.github.com>
2020-12-09 01:56:38 -0500

    Create util.js

---
+++ src/util.js
@@ -0,0 +1,12 @@
+/**
+ * modify a value in place
+ */ 
+export function modify(fn) {
+  return fn(this);
+}
+
+// todo: use import.meta for extra info
+export function dbg() {
+  console.log(this);
+  return this;
+}
